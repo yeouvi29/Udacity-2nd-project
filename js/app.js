0831478e18bc5e58499e1a313afe820246c70660
Emily <yeouvi29@gmail.com>
2021-08-26 08:29:18 -0700

    modify menu display

--- js/app.js
+++ js/app.js
@@ -8,101 +8,106 @@ const FRAGMENT = document.createDocumentFragment();
 /**
  *  Create list elements & append to module:addList~FRAGMENT & add class "nav-li".
  */
-for (let i=0; i<5; i++) {
+const menu = ["Home", "About", "Story", "Likes", "Connect"];
+menu.forEach((menu, i) => {
+  const NEWLIST = document.createElement("li");
 
-    const NEWLIST = document.createElement('li');
+  NEWLIST.innerText = menu;
 
-    NEWLIST.innerText = "SEC " + (i+1);
-    
-    FRAGMENT.appendChild(NEWLIST);
-    NEWLIST.classList.add("nav-li");
-}
+  FRAGMENT.appendChild(NEWLIST);
+  NEWLIST.classList.add("nav-li");
+});
 /**
  * Append module:addList~FRAGMENT to module:addList_NAVUL
  */
-NAVUL.appendChild(FRAGMENT); 
+NAVUL.appendChild(FRAGMENT);
 
 /** @module sectionActive */
 
 /* will be module:sectionActive~SECTIONS */
 const SECTIONS = document.querySelectorAll("section");
 
-/** 
-* @description Make section active
-* @constructor
-* @return Add a class to the active section & remove  a class to other sections.
-*/
+/**
+ * @description Make section active
+ * @constructor
+ * @return Add a class to the active section & remove  a class to other sections.
+ */
 function activeSection() {
-    /** 
-     * Get each section in module:SectionaActive~SECTIONS' boundary position.
-     * Find the section that one's top position is within a range & add a class.
-     * Remove classes from other sections.
-     */
-    for (const section of SECTIONS) {
+  /**
+   * Get each section in module:SectionaActive~SECTIONS' boundary position.
+   * Find the section that one's top position is within a range & add a class.
+   * Remove classes from other sections.
+   */
+  for (const section of SECTIONS) {
+    const sectionPosition = section.getBoundingClientRect();
+    const sectionTop = sectionPosition.top;
 
-        const sectionPosition = section.getBoundingClientRect();
-        const sectionTop = sectionPosition.top;
- 
-        if (sectionTop > -950 && sectionTop <= 0) {
-
-            document.getElementById(section.id).classList.add("highlight");  
-
-        } else {
-            document.getElementById(section.id).classList.remove("highlight");
-
-        }
+    if (sectionTop > -950 && sectionTop <= 0) {
+      document.getElementById(section.id).classList.add("highlight");
+    } else {
+      document.getElementById(section.id).classList.remove("highlight");
     }
+  }
 }
 
 /**
- * @description A event that calls back the function actveSection() when scroll event occurs   
+ * @description A event that calls back the function actveSection() when scroll event occurs
  *
  * @event DOMContentLoadded
  * @event scroll
  * @callback activeSection
- *  
-*/
-document.addEventListener("DOMContentLoaded", function() {
-        document.addEventListener("scroll", function() {
-            setTimeout(activeSection(), 0);
-        });
-
-    });
+ *
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("scroll", function () {
+    setTimeout(activeSection(), 0);
+  });
+});
 
 /** @module clickEvent */
 
 /**
  * @description A list in navigation is clicked, scroll to the designated section
- * 
+ *
  * @event click
  * @fires scroll to the section with id "title"
  */
-document.querySelector(".nav-ul li:nth-child(1)").addEventListener("click", function () {
-    document.getElementById("title").scrollIntoView({behavior: "smooth", block: "start"});
-}); 
+document
+  .querySelector(".nav-ul li:nth-child(1)")
+  .addEventListener("click", function () {
+    document
+      .getElementById("title")
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
+document
+  .querySelector(".nav-ul li:nth-child(2)")
+  .addEventListener("click", function () {
+    document
+      .getElementById("about")
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
-document.querySelector(".nav-ul li:nth-child(2)").addEventListener("click", function () {
-    document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
-});
+document
+  .querySelector(".nav-ul li:nth-child(3)")
+  .addEventListener("click", function () {
+    document
+      .getElementById("story")
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
+document
+  .querySelector(".nav-ul li:nth-child(4)")
+  .addEventListener("click", function () {
+    document
+      .getElementById("like")
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
-document.querySelector(".nav-ul li:nth-child(3)").addEventListener("click", function () {
-    document.getElementById("story").scrollIntoView({behavior: "smooth", block: "start"});
-});
-
-
-document.querySelector(".nav-ul li:nth-child(4)").addEventListener("click", function () {
-    document.getElementById("like").scrollIntoView({behavior: "smooth", block: "start"});
-});
-
-
-document.querySelector(".nav-ul li:nth-child(5)").addEventListener("click", function () {
-    document.getElementById("connect").scrollIntoView({behavior: "smooth", block: "start"});
-});
-    
-
-
-
-
-
+document
+  .querySelector(".nav-ul li:nth-child(5)")
+  .addEventListener("click", function () {
+    document
+      .getElementById("connect")
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
